53fa4bcda9bf86acac118d40ad61da7f3cd1522a
breno.mcosta <breno.mcosta@pbh.gov.br>
2023-12-27 22:34:49 -0300

    up

--- src/main/java/com/br/aws/ecommerce/layers/repository/ProductRepository.java
+++ src/main/java/com/br/aws/ecommerce/layers/repository/ProductRepository.java
@@ -9,11 +9,11 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.ItemCollection;
-import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
 import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.dynamodbv2.model.ReturnValue;
 import com.br.aws.ecommerce.layers.base.BaseLambdaFunction;
@@ -27,66 +27,71 @@ public class ProductRepository extends BaseLambdaFunction {
 	private DynamoDB dynamoDB;
 
 	private String tableProducts = "product";
-	
-	 
 
 	public ProductRepository(AmazonDynamoDB amazonDynamoDB) {
 		this.dynamoDB = new DynamoDB(amazonDynamoDB);
 	}
-	
-	
+
+	 
 	public ProductDTO update(ProductDTO product, String uuid) {
-		
-	      try {
-	    	  
-	    	  final Table table = this.dynamoDB.getTable(this.tableProducts);
- 
-	          final UpdateItemSpec updateItemSpec = new UpdateItemSpec() 
-	            .withPrimaryKey("id", uuid)
-	            .withUpdateExpression("set name = :name, code=:code, price=:price, model=:model")
-	              .withValueMap(new ValueMap() 
-	              .withString(":name", product.getName())
-	              .withString(":code", product.getCode())
-	              .withString(":price", product.getPrice())
-                  .withString(":model", product.getModel())) 
-	            .withConditionExpression("id = :id")
-	              .withValueMap(new ValueMap() 
-	    	      .withString(":id", uuid))
-	            .withReturnValues(ReturnValue.UPDATED_NEW);  
-	          
-	          UpdateItemOutcome updateItemOutcome =  table.updateItem(updateItemSpec);  
-	         
-	          return super.getMapper().readValue(updateItemOutcome.getItem().toJSONPretty(), ProductDTO.class);     
-	      } catch (Exception e) { 
-	    	 this.logger.log(Level.SEVERE, "Cannot update product: %s" , e.getMessage());
-	    	 throw new RuntimeException(e); 
-	      }         
-		
-	}
-	
-	public ProductDTO save(ProductDTO product) {
-		
+
 		try {
-	
-		  final Table table = this.dynamoDB.getTable(this.tableProducts);
-		
-		  final Item item = new Item() 
-		            .withPrimaryKey("id", UUID.randomUUID().toString())
-		            .withString("name", product.getName())
-		            .withString("code", product.getCode())
-		            .withString("price", product.getPrice())
-		            .withString("model", product.getModel());
-		
-		  final PutItemOutcome putItemOutcome  = table.putItem(item);  
-		        
-		  return super.getMapper().readValue(putItemOutcome.getItem().toJSONPretty(), ProductDTO.class);
-		
+
+			final Table table = this.dynamoDB.getTable(this.tableProducts);
+
+			final UpdateItemSpec updateItemSpec = new UpdateItemSpec()
+					.withPrimaryKey("id", uuid)
+					.withUpdateExpression("set #name = :val1, #code = :val2, #price = :val3, #model = :val4")
+					.withConditionExpression("attribute_exists(id)")
+					.withNameMap(
+							new NameMap().with("#name", "name")
+				                          .with("#code", "code")
+						                  .with("#price", "price")
+						                  .with("#model", "model"))
+					.withValueMap(
+							new ValueMap().withString(":val1", product.getName())
+							              .withString(":val2", product.getCode())
+									      .withString(":val3", product.getPrice())
+									      .withString(":val4", product.getModel()))
+					.withReturnValues(ReturnValue.ALL_NEW);
+
+			UpdateItemOutcome updateItemOutcome = table.updateItem(updateItemSpec);
+
+			return super.getMapper().readValue(updateItemOutcome.getItem().toJSON(), ProductDTO.class);
 		} catch (Exception e) {
-			this.logger.log(Level.SEVERE, "Cannot save product: %s" , e.getMessage());
+			this.logger.log(Level.SEVERE, String.format("Cannot update product: %s", e.getMessage()),e);
 			throw new RuntimeException(e);
-		}  
+		}
+
 	}
-	
+
+
+	 
+	public ProductDTO save(ProductDTO product) {
+
+		try {
+
+			final Table table = this.dynamoDB.getTable(this.tableProducts);
+
+			final String id = UUID.randomUUID().toString();
+
+			final Item item = new Item().withPrimaryKey("id", id).withString("name", product.getName())
+					.withString("code", product.getCode()).withString("price", product.getPrice())
+					.withString("model", product.getModel());
+
+			table.putItem(item);
+
+			product.setId(id);
+
+			return product;
+
+		} catch (Exception e) {
+			this.logger.log(Level.SEVERE, String.format("Cannot save product: %s", e.getMessage()), e);
+			throw new RuntimeException(e);
+		}
+	}
+
+	 
 	public void delete(String uuid) {
 
 		try {
@@ -95,12 +100,13 @@ public class ProductRepository extends BaseLambdaFunction {
 			table.deleteItem("id", uuid);
 
 		} catch (Exception e) {
-			this.logger.log(Level.SEVERE, "Cannot delete product: %s", e.getMessage());
+			this.logger.log(Level.SEVERE, String.format("Cannot delete product: %s", e.getMessage()),e);
 			throw new RuntimeException(e);
 		}
 
 	}
 
+	 
 	public List<ProductDTO> findAll() {
 
 		try {
@@ -109,21 +115,28 @@ public class ProductRepository extends BaseLambdaFunction {
 
 			final ItemCollection<ScanOutcome> itemCollection = table.scan();
 
-			String itensStr = "";
+			final StringBuilder sb = new StringBuilder();
+			
+			sb.append("[");
 
 			for (Item item : itemCollection) {
-				itensStr += item.toJSONPretty() + ",";
+				sb.append(item.toJSON()).append(",");
 			}
-
-			return super.getMapper().readValue(itensStr, new TypeReference<List<ProductDTO>>() {});
+			
+			sb.append("]");
+			
+			 
+		   return super.getMapper().readValue(sb.toString().replace(",]", "]"), new TypeReference<List<ProductDTO>>() {});
+			 
 
 		} catch (Exception e) {
-			this.logger.log(Level.SEVERE, "Cannot find all products: %s" , e.getMessage());
+			this.logger.log(Level.SEVERE, String.format("Cannot find all products: %s", e.getMessage()), e);
 			throw new RuntimeException(e);
-		}  
+		}
 
 	}
 
+	 
 	public ProductDTO findById(String id) {
 
 		try {
@@ -131,12 +144,17 @@ public class ProductRepository extends BaseLambdaFunction {
 			final Table table = this.dynamoDB.getTable(this.tableProducts);
 
 			final Item item = table.getItem("id", id);
+			
+			if (item == null) {
+				throw new RuntimeException("Product not found!");
+			}
 
-			return super.getMapper().readValue(item.toJSONPretty(), ProductDTO.class);
+			return super.getMapper().readValue(item.toJSON(), ProductDTO.class);
 		} catch (Exception e) {
-			this.logger.log(Level.SEVERE, "Cannot find product by id: %s" , e.getMessage());
+			this.logger.log(Level.SEVERE, String.format("Cannot product by id: %s", e.getMessage()),e);
 			throw new RuntimeException(e);
-		} 
+		}
 	}
-
+	
+	
 }
